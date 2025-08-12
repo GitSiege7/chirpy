a126ecb6b0623e24ab8f87f69c8a3219bf310105
GitSiege7 <carljklug@icloud.com>
2025-08-12 17:05:43 -0400

    custom/stateful handlers

--- main.go
+++ main.go
@@ -1,6 +1,40 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func handlerReadiness(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerCount(w http.ResponseWriter, r *http.Request) {
+	count := cfg.fileserverHits.Load()
+
+	fmt.Fprintf(w, "Hits: %v", count)
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -9,9 +43,16 @@ func main() {
 		Addr:    ":8080",
 	}
 
-	file_server := http.FileServer(http.Dir("/home/siege/workspace/Go/chirpy/"))
+	cfg := &apiConfig{}
+	cfg.fileserverHits.Store(0)
 
-	mux.Handle("/", file_server)
+	filepathRoot := "/home/siege/workspace/Go/chirpy/"
+
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", cfg.handlerCount)
+	mux.HandleFunc("/reset", cfg.handlerReset)
+
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 
 	server.ListenAndServe()
 }
